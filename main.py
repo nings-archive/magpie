20ba73f97e362d4f83f503524968d0dbce800cc8
ning <ningyuan.sg@gmail.com>
2016-12-15 00:31:52 +0800

    Adds new class for tracking html changes
    The new class HTML_Update_Service will check html from a URL. It
    will find the first chunk of text that fits the bs4 selector and
    filter text (if any), and update Magpie if there has been any
    change in said text.
    
    Additionally, service classes (i.e. twitch, html), now take a
    dictionary generated from config['services'] as the __init__()
    argument. This is simply better than the previous system where
    string variables from the relevant chunk of json would be
    individually passed to __init__().
    
    Some variable names were also change and added to config.json in
    this commit. Of note is the service-name, which was added for
    consistency between twitch and html services.

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ if not os.path.isfile(PATH_CONFIG_JSON):
                 "telegram_bot_token":"",
                 "telegram_channel_public_link":"",
                 "telegram_channel_id":"",
-                "twitch_client_key":""
+                "twitch_client_key":"",
+                "facebook_app_id":""
             },
              "services": [
                     {
@@ -68,14 +69,17 @@ class Magpie_Bot:
 class Twitch_Service:
     '''for channels on twitch
     '''
-    def __init__(self, channel):
-        self.channel = channel
+    def __init__(self, service_json):
+        self.service_name = service_json['service-name']
+        self.channel = service_json['channel']
+        self.history = service_json['history']
         self.client_key = config['accounts']['twitch_client_id']
         self.query_url = '{api_url}{channel}?client_id={key}'.format(
                 api_url=TWITCH_API_URL,
                 channel=self.channel,
                 key=config['accounts']['twitch_client_id']
                 )
+        # gets is_streaming from api.twitch (query_url)
         self.json_data = json.loads(requests.get(self.query_url).text)
         if self.json_data['stream'] is None:
             self.is_streaming = False
@@ -83,13 +87,33 @@ class Twitch_Service:
             self.is_streaming = True
 
 
+class HTML_Update_Service:
+    '''for html sites
+    '''
+    def __init__(self, item):
+        self.url = item['url']
+        self.service_name = item['service-name']
+        self.selector = item['selector']
+        self.filt = item['filter']
+        self.history = item['history']
+        self.soup = bs4.BeautifulSoup(requests.get(self.url).text, 'html.parser')
+        self.content = self.soup.find_all(attrs=self.selector)
+        for item in self.content:
+            text = item.get_text()
+            if self.filt in text:
+                self.requests_text = text
+                break
+
 def main():
     magpie = Magpie_Bot()
 
     for item in config['services']:
         if item['service'] == 'twitch':
-            flight = Twitch_Service(item['channel'])
-            if flight.is_streaming is not item['history']:
+            flight = Twitch_Service(item)
+            if flight.is_streaming is not flight.history:
+                # access item['history'] instead of flight.history
+                # in order to change config dict object, so as to
+                # write to config.json
                 item['history'] = flight.is_streaming
                 write_config(config)
                 if flight.is_streaming:
@@ -97,9 +121,22 @@ def main():
                 else:
                     status_word = 'stopped'
                 magpie.post_update('''
-<b>Twitch/{channel}</b>
+<b>{service_name}</b>
 {channel} has just {status} streaming
-                '''.format(channel=item['channel'], status=status_word))
+                '''.format(service_name=flight.service_name,
+                    channel=flight.channel,
+                    status=status_word))
+        if item['service'] == 'html':
+            flight = HTML_Update_Service(item)
+            if not flight.requests_text == flight.history:
+                item['history'] = flight.requests_text
+                write_config(config)
+                magpie.post_update('''
+                
+<b>{service_name}</b>
+{text_body}
+                '''.format(service_name=flight.service_name,
+                           text_body=flight.requests_text))
                 
 if __name__ == '__main__':
     main()
