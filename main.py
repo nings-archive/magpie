001cb8611a67e468ef15cdd1e8cbea6ed3985026
ning <ningyuan.sg@gmail.com>
2016-12-14 20:29:03 +0800

    Add code that processes and tweets twitch streams
    This is handled by the class Twitch_Service. Some notes:
    - More changes were made to variable names, within main.py and
      config.json. This is to make them more obviously descriptive.
    - Each Twitch_Service object is created with the variable name
      'flight'
    - History (if channel was streaming since last check) is stored in
      config.json under key 'history' for ease of use. Rationale being
      one json file is better than two, especially when this property
      can be stored in the same dictionary as the channel it
      references.
    - Different twitch channels are stored in an array (list?)
      belonging to the key 'services'
    - The set-up generation of config.json has been modified to
      reflect this change to config.json
    - parse_mode='HTML' has been added to Magpie_Bot.post_update() to
      allow simple formatting of messages. I preferred to use markdown,
      but that was throwing errors. It also seems from googling that
      there is consensus that html works better than markdown

--- main.py
+++ main.py
@@ -13,19 +13,27 @@ logging.basicConfig(level=logging.INFO,
 # CONSTANTS
 PATH = sys.path[0] + '/'
 PATH_CONFIG_JSON = PATH + 'config.json'
-CHANNEL_ID_QUERY = '{"ok":false,"error_code":401,"description":"Unauthorized"}'
+PATH_HISTORY_JSON = PATH + 'history.json'
+TELEGRAM_CHANNEL_ID_QUERY = '{"ok":false,"error_code":401,"description":"Unauthorized"}'
+TWITCH_API_URL = 'https://api.twitch.tv/kraken/streams/'
 
 # CONFIG.JSON
 if not os.path.isfile(PATH_CONFIG_JSON):
     with open(PATH_CONFIG_JSON, 'w') as file:
         json.dump(
-            {"accounts":
-                {"telegram_bot_token":"",
-                 "telegram_channel_public_link":"",
-                 "telegram_channel_id":""},
-             "services":
-                {"site":"",
-                 "url":""}},
+            {"accounts": {
+                "telegram_bot_token":"",
+                "telegram_channel_public_link":"",
+                "telegram_channel_id":"",
+                "twitch_client_key":""
+            },
+             "services": [
+                    {
+                       "site":"",
+                       "url":""
+                    }
+                ]
+            },
             file,
             indent=4
         )
@@ -37,6 +45,7 @@ def write_config(config):
     with open(PATH_CONFIG_JSON, 'w') as file:
         json.dump(config, file, indent=4)
 
+        
 # CLASSES
 class Magpie_Bot:
     '''magpie is the class for the telegram api
@@ -47,9 +56,51 @@ class Magpie_Bot:
         self.channel_public_link = config['accounts']['telegram_channel_public_link']
         self.channel_id = config['accounts']['telegram_channel_id']
         if self.channel_id == '':
-            query = self.bot.sendMessage(chat_id=self.channel_public_link, text=CHANNEL_ID_QUERY)
+            query = self.bot.sendMessage(chat_id=self.channel_public_link, text=TELEGRAM_CHANNEL_ID_QUERY)
             self.channel_id = query.chat.id
             config['accounts']['telegram_channel_id'] = self.channel_id
             write_config(config)
+    
+    def post_update(self, message):
+        self.bot.sendMessage(chat_id=self.channel_id, parse_mode='HTML', text=message)
 
-magpie = Magpie_Bot()
+
+class Twitch_Service:
+    '''for channels on twitch
+    '''
+    def __init__(self, channel):
+        self.channel = channel
+        self.client_key = config['accounts']['twitch_client_id']
+        self.query_url = '{api_url}{channel}?client_id={key}'.format(
+                api_url=TWITCH_API_URL,
+                channel=self.channel,
+                key=config['accounts']['twitch_client_id']
+                )
+        self.json_data = json.loads(requests.get(self.query_url).text)
+        if self.json_data['stream'] is None:
+            self.is_streaming = False
+        else:
+            self.is_streaming = True
+
+
+def main():
+    magpie = Magpie_Bot()
+
+    for item in config['services']:
+        if item['service'] == 'twitch':
+            flight = Twitch_Service(item['channel'])
+            if flight.is_streaming is not item['history']:
+                item['history'] = flight.is_streaming
+                write_config(config)
+                if flight.is_streaming:
+                    status_word = 'started'
+                else:
+                    status_word = 'stopped'
+                magpie.post_update('''
+<b>Twitch/{channel}</b>
+{channel} has just {status} streaming
+                '''.format(channel=item['channel'], status=status_word))
+                
+if __name__ == '__main__':
+    main()
+
