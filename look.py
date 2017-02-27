7ea3a30f8f109ad2bd9672c26aa086fc44e09642
ning <ningyuan.sg@gmail.com>
2017-02-27 23:21:57 +0800

    Change structure to reduce time between obj intiation & feed lookup
    This is an attempt to fix a bug where a duplicate of the latest
    dota2 match would be sent if /streams was called shortly after the
    first.

--- look.py
+++ look.py
@@ -5,19 +5,16 @@ import magpie.core, magpie.twitch, magpie.rss, magpie.dota2
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
-# INITIALISATION
-core    = magpie.core.Core()
-twitch  = magpie.twitch.Twitch()
-rss     = magpie.rss.Rss()
-dota2   = magpie.dota2.Dota2()
-
 # TWITCH
+twitch = magpie.twitch.Twitch()
 if twitch.realtime_enabled:
     twitch.send_new_updates()
 
 # RSS
+rss = magpie.rss.Rss()
 rss.send_new_updates()
 
 # DOTA2
+dota2   = magpie.dota2.Dota2()
 dota2.send_latest_match()
 
