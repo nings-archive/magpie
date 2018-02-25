a05448cdc25b70561f310e34b91a5afe4a5e9c38
ning <ningyuan.sg@gmail.com>
2018-02-25 23:31:52 +0800

    Add commands, starting with LSM module helpers

--- magpie/__main__.py
+++ magpie/__main__.py
@@ -1,8 +1,11 @@
-import configparser
+import configparser, logging
 from bot import Bot
+
+logging.basicConfig(level=logging.INFO)
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 bot = Bot(config['telegram']['token'], config['telegram']['my_id'])
-bot.send_me('Hello world!')
+bot.send_me('<code>Poo-tee-weet?</code>')
+bot.listen()
--- magpie/bot.py
+++ magpie/bot.py
@@ -1,14 +1,46 @@
-import telegram
+import logging
+import telegram, telegram.ext
 import lsm_helpers
+
+CommandHandler = telegram.ext.CommandHandler
 
 class Bot(telegram.Bot):
     def __init__(self, token, my_id):
         telegram.Bot.__init__(self, token=token)
         self.my_id = my_id
 
+    def listen(self):
+        self.updater = telegram.ext.Updater(bot=self)
+        self.dispatcher = self.updater.dispatcher
+        self.dispatcher.add_handler(CommandHandler('get_random_dsDNA', 
+            self.get_random_dsDNA, pass_args=True))
+        self.updater.start_polling()
+        self.updater.idle()
+
+    def get_random_dsDNA(self, bot, update, args):
+        if args == []: args = [7]  # set a default value
+        # try:
+        length = int(args[0])
+        seq = lsm_helpers.get_random_dsDNA(length)
+        text = "<code>3'--{}--5'\n5'--{}--3'</code>".format(
+            ''.join(seq[0]), ''.join(seq[1]))
+        self.send(update.message.chat_id, text)
+        # except Exception as error:
+        #    text = "<code>ERR: {}</code>".format(str(error))
+        #    self.send(update.message.chat_id, text)
+        #    logging.info(error)  # raise it to logging anyway
+                
+
     def send_me(self, text):
         return super().send_message(
-            chat_id = self.my_id,
+            chat_id=self.my_id,
+            text=text,
+            parse_mode=telegram.ParseMode.HTML
+        )
+
+    def send(self, chat_id, text):
+        return super().send_message(
+            chat_id=chat_id,
             text=text,
             parse_mode=telegram.ParseMode.HTML
         )
