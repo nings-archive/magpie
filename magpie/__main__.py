f6f0ab2879054a79457fe81bf6d57f6abaf1f43a
ning <ningyuan.sg@gmail.com>
2018-05-24 01:55:33 +0800

    Rename bot as core
    Also make __main__ use `import core` rather than `from core import Bot`,
    because the former allows for a more explicit `core.Bot` reference (as
    compared to `Bot`---what kind of Bot?)

--- magpie/__main__.py
+++ magpie/__main__.py
@@ -1,11 +1,12 @@
 import configparser, logging
-from bot import Bot
+
+import core
 
 logging.basicConfig(level=logging.INFO)
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-bot = Bot(config['telegram']['token'], config['telegram']['my_id'])
+bot = core.Bot(config['telegram']['token'], config['telegram']['my_id'])
 bot.send_me('<code>Poo-tee-weet?</code>')
 bot.listen()
